32b9fc84d33a084a6210032d348a9d8b5dfa886c
RyanMitch16 <ryanmitch16@gmail.com>
2015-04-19 17:10:58 -0500

    Updated Main Class

--- assignment4/src/Main.java
+++ assignment4/src/Main.java
@@ -1,13 +1,28 @@
-package chocAn;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to ChocAn!");
-		System.out.println("1. Provider\n2. Operator\n3. Manager\n4. Member\nEnter choice: ");
-		Scanner input = new Scanner(System.in);
+
+    //The file names for all the databases
+    private static final String memberDatabaseFile = "member_database.dat";
+    private static final String providerDatabaseFile = "provider_database.dat";
+    private static final String serviceDatabaseFile = "service_database.dat";
+    private static final String providedServiceDatabaseFile = "provided_service_database.dat";
+
+    //The databases
+    public static final Database<Member> memberDatabase = new Database<Member>(memberDatabaseFile);
+    public static final Database<Provider> providerDatabase = new Database<Provider> (providerDatabaseFile);
+    public static final Database<Service> serviceDatabase = new Database<Service>(serviceDatabaseFile);
+    public static final Database<ProvidedService> providedServiceDatabase = new Database<ProvidedService>(providedServiceDatabaseFile);
+
+    public static void main(String []args) {
+
+
+        System.out.println("Welcome to ChocAn!");
+        System.out.println("1. Provider\n2. Operator\n3. Manager\n4. Member\nEnter choice: ");
+        Scanner input = new Scanner(System.in);
         int choice = input.nextInt();
         switch (choice) {
             case 1:
@@ -25,6 +40,45 @@ public class Main {
             default:
                 System.out.println("Sorry, not a correct number entered :(");
                 break;
-		}
-	}
+        }
+
+        }
+
+        //Example for quereying information
+        //Get all services provided to member #0
+        /*
+            int memberId = 0;
+            ArrayList<ProvidedService> listOfServices = new ArrayList<ProvidedService>();
+            for (int i=0;i<dbProvidedService.size();i++){
+
+                ProvidedService service = dbProvidedService.getEntry(i);
+                if (service.getMemberId() == memberId){
+                    listOfServices.add(service);
+                }
+            }
+        */
+
+        //Example get an entry's id
+        /*
+            Member m4 = new Member("Ryan", "OK","Address", "City", "Zip", "State");
+            int memberId = dbMember.addEntry(m4);
+
+
+            Member m5 = dbMember.getEntry(0);
+            int memberId2;
+            try{
+                memberId2 = m5.getId();}
+            catch (Exception e){
+                System.out.println(e);
+            }
+        */
+
+        //Example updating an entry
+        /*
+            Member mem = dbMember.getEntry(0);
+            mem.setName("New Name");
+            dbMember.updateEntry(mem);
+         */
+
+    }
 }
