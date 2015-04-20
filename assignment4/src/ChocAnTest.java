fe99fc317a78bac254fae587f1a104514674bb5d
Breanna Tucker <breannatucker@Breannas-MacBook-Pro.local>
2015-04-20 16:29:57 -0500

    got the test working yayyy

--- assignment4/src/ChocAnTest.java
+++ assignment4/src/ChocAnTest.java
@@ -6,7 +6,8 @@ public class ChocAnTest {
   private Provider p;
   private Member m;
   private Service s;
-  private ChocAnMain c;
+  //private ChocAnMain c;
+  private String memberDatabaseFile = "member_database.dat";
   public Database<Member> memberDatabase = new Database<Member>(memberDatabaseFile);
   
   @Before
@@ -14,7 +15,8 @@ public class ChocAnTest {
     p = new Provider("Rachel Smith", "456 Carol Street", "Sutton", "56789", "MA");
     m = new Member("Jacob Farmer", "Suspended", "123 Maple Street", "Farmington", "12345", "WI");
     s = new Service("talk for a while", 4200);
-    c = new ChocAnMain();
+    //c = new ChocAnMain();
+    memberDatabase.clear();
   }
   
   @Test
@@ -34,19 +36,19 @@ public class ChocAnTest {
     double fee = 99.99;
     s.setFee(fee);
     
-    assertEquals("99.99", s.getFee());
+    assertEquals(99.99,fee, s.getFee());
   }
     
   @Test
   public void testDatabase() {
-    Member mem1 = new Member (Josh Turner", "OK", "625 Adams Street", "Orlando", "34443", "CA");
+    Member mem1 = new Member ("Josh Turner", "OK", "625 Adams Street", "Orlando", "34443", "CA");
     int memberId = memberDatabase.addEntry(mem1);
     
     memberDatabase = new Database<Member>(memberDatabaseFile);
     
     Member mem2 = memberDatabase.getEntry(0);
     try{
-      assertEquals("0", mem2.getId());
+      assertEquals(0, mem2.getId());
     }
     catch (Exception e){
       System.out.println(e);
@@ -55,14 +57,16 @@ public class ChocAnTest {
   
   @Test
   public void testUpdate() {
-    memberDatabase = new Database<Member>(memberDatabaseFile);
+	Member mem1 = new Member ("Turner Bob", "OK", "343 fulton street", "Tacoma", "34657", "MI");
+	int memberId = memberDatabase.addEntry(mem1);
+	memberDatabase = new Database<Member>(memberDatabaseFile);
     Member mem3 = memberDatabase.getEntry(0);
     mem3.setName("Adam Clark");
     memberDatabase.updateEntry(mem3);
     
     Member mem4 = memberDatabase.getEntry(0);
     try{
-      assertEquals("0", mem4.getId());
+      assertEquals(0, mem4.getId());
     }
     catch (Exception e) {
       System.out.println(e);
