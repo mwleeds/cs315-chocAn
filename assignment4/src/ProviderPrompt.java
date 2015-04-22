95a1e49d9c07b37dc6f6c381d40d65f96e720869
mleeds95 <mwl458@gmail.com>
2015-04-21 20:29:35 -0500

    Implement billing ChocAn section of ProviderPrompt.

--- assignment4/src/ProviderPrompt.java
+++ assignment4/src/ProviderPrompt.java
@@ -1,6 +1,10 @@
 import java.io.*;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 import database.*;
 
 /**
@@ -10,6 +14,7 @@ import database.*;
 public class ProviderPrompt extends Prompter {
 	
 	public void run() {
+        goForward("Provider");
         String id = prompt("Enter ID: ");
 		if (id.length() != 9) {
 			System.out.println("Invalid ID");
@@ -22,45 +27,82 @@ public class ProviderPrompt extends Prompter {
             System.out.println("Invalid ID");
             return;
         }
-        String choice = prompt("1. Access Provider Directory\n2. Bill ChocAn for Service\n3. Request Report\nEnter choice: ");
-        switch (choice) {
-            case "1":
-                ArrayList<Service> services = ChocAnMain.providerDirectoryDatabase.getEntryList();
-                System.out.println("Provider Directory");
-                for (int i = 0; i < services.size(); ++i) {
-                    Service s = services.get(i);
-                    System.out.println("Name: " + s.getName());
-                    try { System.out.println("Number: " + s.getId()); } catch (Exception e) {}
-                    System.out.println("Fee: " + s.getFee());
-                }
-                break;
-            case "2":
-                System.out.println("Provider Database");
-                //TODO
-                break;
-            case "3":
-                String filename = prompt("Enter report file name?");
-                File reportFile = new File(filename);
-                if (reportFile.exists()) {
-                    String response = prompt("File exists. Overwrite? [Y/n]");
-                    if (response.substring(0,0).toUpperCase().equals("N"))
+        String choice = "";
+        while (!choice.equals("4")) {
+            choice = prompt("Enter a number to choose what operation to perform\n1. Access Provider Directory\n2. Bill ChocAn for Service\n3. Request Report\n4. To go back");
+            switch (choice) {
+                case "1":
+                    goForward("Access Provider Directory");
+                    ArrayList<Service> services = ChocAnMain.providerDirectoryDatabase.getEntryList();
+                    for (int i = 0; i < services.size(); ++i) {
+                        Service s = services.get(i);
+                        System.out.println("Name: " + s.getName());
+                        try { System.out.println("Number: " + s.getId()); } catch (Exception e) {}
+                        System.out.println("Fee: " + s.getFee());
+                    }
+                    break;
+                case "2":
+                    goForward("Bill ChocAn for Service");
+                    String memberId = prompt("Enter Member ID: ");
+                    if (memberId.length() != 9) {
+                        System.out.println("Error: Member IDs must be 9 digits long!");
+                        break;
+                    }
+                    Member thisMember = ChocAnMain.memberDatabase.getEntry(Integer.parseInt(memberId));
+                    if (thisMember == null) {
+                        System.out.println("No member found for ID " + memberId);
+                        break;
+                    }
+                    String strDate = prompt("Enter date service provided (MM-DD-YYYY): ");
+                    DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+                    Date dateProvided;
+                    try { 
+                        dateProvided = dateFormat.parse(strDate);
+                    } catch (ParseException e) { 
+                        System.out.println("Error parsing date " + strDate);
+                        break;
+                    }
+                    int serviceId = Integer.parseInt(prompt("Enter service ID: "));
+                    Service thisService = ChocAnMain.providerDirectoryDatabase.getEntry(serviceId);
+                    if (thisService == null) {
+                        System.out.println("Error: Invalid service ID provided.");
+                        break;
+                    }
+                    System.out.println("Provided service: " + thisService.getName());
+                    String confirm = prompt("Is this correct? [Y/n]");
+                    if (confirm.substring(0,0).toUpperCase().equals("N")) break;
+                    String comments = "";
+                    comments = prompt("Comments (optional): ");
+                    ProvidedService pS = new ProvidedService(dateProvided, Integer.parseInt(memberId), Integer.parseInt(id), serviceId, comments);
+                    ChocAnMain.providedServiceDatabase.addEntry(pS);
+                    prompt("Record successfully written to the database. Press enter to continue");
+                    break;
+                case "3":
+                    goForward("Request Report");
+                    String filename = prompt("Enter report file name?");
+                    File reportFile = new File(filename);
+                    if (reportFile.exists()) {
+                        String response = prompt("File exists. Overwrite? [Y/n]");
+                        if (response.substring(0,0).toUpperCase().equals("N"))
+                            return;
+                        else 
+                            reportFile.delete();
+                    }
+                    try {
+                        reportFile.createNewFile();
+                        ProviderReport pReport = new ProviderReport(reportFile, thisProvider);
+                        pReport.generateReport();
+                    } catch (IOException e) {
+                        System.out.println("Error writing to file " + filename);
+                        System.out.println(e.getMessage());
                         return;
-                    else 
-                        reportFile.delete();
-                }
-                try {
-                    reportFile.createNewFile();
-                    ProviderReport pReport = new ProviderReport(reportFile, thisProvider);
-                    pReport.generateReport();
-                } catch (IOException e) {
-                    System.out.println("Error writing to file " + filename);
-                    System.out.println(e.getMessage());
-                    return;
-                }
-                break;
-            default:
-                System.out.println("Sorry, not a correct number entered :(");
-                break;
-        }
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid selection");
+                    break;
+            }
+        } // end while
+        goBack();
 	}
 }
